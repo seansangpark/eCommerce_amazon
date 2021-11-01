bea312eacac9efc0a8275832c7964d64ca07c723
Sean Park <seansangpark@gmail.com>
2021-10-31 19:41:32 -0700

    ProductScreen component created

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function ProductScreen() {
-  return <div>Product Screen</div>;
+  return (
+    <div>
+      <div className='row'>
+        <div className='col-2'></div>
+        <div className='col-1'></div>
+        <div className='col-1'></div>
+      </div>
+    </div>
+  );
 }
